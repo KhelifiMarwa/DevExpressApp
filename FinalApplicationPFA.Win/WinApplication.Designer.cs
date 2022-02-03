2f21cd4ba107d3ff709afc3234a4c559ea221d3d
KhelifiMarwa <marwa.khelifi@polytechnicien.tn>
2022-02-03 12:25:59 +0100

    gestion des Ressources humaines et des Ventes

---
+++ FinalApplicationPFA.Win/WinApplication.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace FinalApplicationPFA.Win {
+    partial class FinalApplicationPFAWindowsFormsApplication {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if(disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.module1 = new DevExpress.ExpressApp.SystemModule.SystemModule();
+            this.module2 = new DevExpress.ExpressApp.Win.SystemModule.SystemWindowsFormsModule();
+            this.module3 = new FinalApplicationPFA.Module.FinalApplicationPFAModule();
+            this.module4 = new FinalApplicationPFA.Module.Win.FinalApplicationPFAWindowsFormsModule();
+            this.securityModule1 = new DevExpress.ExpressApp.Security.SecurityModule();
+            this.securityStrategyComplex1 = new DevExpress.ExpressApp.Security.SecurityStrategyComplex();
+            this.securityStrategyComplex1.SupportNavigationPermissionsForTypes = false;
+            this.conditionalAppearanceModule = new DevExpress.ExpressApp.ConditionalAppearance.ConditionalAppearanceModule();
+            this.dashboardsModule = new DevExpress.ExpressApp.Dashboards.DashboardsModule();
+            this.dashboardsWindowsFormsModule = new DevExpress.ExpressApp.Dashboards.Win.DashboardsWindowsFormsModule();
+            this.fileAttachmentsWindowsFormsModule = new DevExpress.ExpressApp.FileAttachments.Win.FileAttachmentsWindowsFormsModule();
+            this.notificationsModule = new DevExpress.ExpressApp.Notifications.NotificationsModule();
+            this.notificationsWindowsFormsModule = new DevExpress.ExpressApp.Notifications.Win.NotificationsWindowsFormsModule();
+            this.stateMachineModule = new DevExpress.ExpressApp.StateMachine.StateMachineModule();
+            this.treeListEditorsModuleBase = new DevExpress.ExpressApp.TreeListEditors.TreeListEditorsModuleBase();
+            this.treeListEditorsWindowsFormsModule = new DevExpress.ExpressApp.TreeListEditors.Win.TreeListEditorsWindowsFormsModule();
+            this.validationModule = new DevExpress.ExpressApp.Validation.ValidationModule();
+            this.validationWindowsFormsModule = new DevExpress.ExpressApp.Validation.Win.ValidationWindowsFormsModule();
+            this.viewVariantsModule = new DevExpress.ExpressApp.ViewVariantsModule.ViewVariantsModule();
+            this.authenticationStandard1 = new DevExpress.ExpressApp.Security.AuthenticationStandard();
+            ((System.ComponentModel.ISupportInitialize)(this)).BeginInit();
+            // 
+            // securityStrategyComplex1
+            // 
+            this.securityStrategyComplex1.Authentication = this.authenticationStandard1;
+            this.securityStrategyComplex1.RoleType = typeof(DevExpress.Persistent.BaseImpl.PermissionPolicy.PermissionPolicyRole);
+            // ApplicationUser descends from PermissionPolicyUser and supports OAuth authentication. For more information, refer to the following help topic: https://docs.devexpress.com/eXpressAppFramework/402197
+            // If your application uses PermissionPolicyUser or a custom user type, set the UserType property as follows:
+            this.securityStrategyComplex1.UserType = typeof(FinalApplicationPFA.Module.BusinessObjects.ApplicationUser);
+            // 
+            // securityModule1
+            // 
+            this.securityModule1.UserType = typeof(FinalApplicationPFA.Module.BusinessObjects.ApplicationUser);
+            // 
+            // authenticationStandard1
+            // 
+            this.authenticationStandard1.LogonParametersType = typeof(DevExpress.ExpressApp.Security.AuthenticationStandardLogonParameters);
+            // ApplicationUserLoginInfo is only necessary for applications that use the ApplicationUser user type.
+            // Comment out the following line if using PermissionPolicyUser or a custom user type.
+            this.authenticationStandard1.UserLoginInfoType = typeof(FinalApplicationPFA.Module.BusinessObjects.ApplicationUserLoginInfo);
+            //
+            // dashboardsModule
+            //
+            this.dashboardsModule.DashboardDataType = typeof(DevExpress.Persistent.BaseImpl.DashboardData);
+            this.dashboardsWindowsFormsModule.DesignerFormStyle = DevExpress.XtraBars.Ribbon.RibbonFormStyle.Ribbon;
+            //
+            // stateMachineModule
+            //
+            this.stateMachineModule.StateMachineStorageType = typeof(DevExpress.ExpressApp.StateMachine.Xpo.XpoStateMachine);
+            //
+            // validationModule
+            //
+            this.validationModule.AllowValidationDetailsAccess = false;
+            // 
+            // FinalApplicationPFAWindowsFormsApplication
+            // 
+            this.ApplicationName = "FinalApplicationPFA";
+            this.CheckCompatibilityType = DevExpress.ExpressApp.CheckCompatibilityType.DatabaseSchema;
+            this.Modules.Add(this.module1);
+            this.Modules.Add(this.module2);
+            this.Modules.Add(this.module3);
+            this.Modules.Add(this.module4);
+            this.Modules.Add(this.securityModule1);
+            this.Security = this.securityStrategyComplex1;
+            this.Modules.Add(this.conditionalAppearanceModule);
+            this.Modules.Add(this.dashboardsModule);
+            this.Modules.Add(this.dashboardsWindowsFormsModule);
+            this.Modules.Add(this.fileAttachmentsWindowsFormsModule);
+            this.Modules.Add(this.notificationsModule);
+            this.Modules.Add(this.notificationsWindowsFormsModule);
+            this.Modules.Add(this.stateMachineModule);
+            this.Modules.Add(this.treeListEditorsModuleBase);
+            this.Modules.Add(this.treeListEditorsWindowsFormsModule);
+            this.Modules.Add(this.validationModule);
+            this.Modules.Add(this.validationWindowsFormsModule);
+            this.Modules.Add(this.viewVariantsModule);
+            this.UseOldTemplates = false;
+            this.DatabaseVersionMismatch += new System.EventHandler<DevExpress.ExpressApp.DatabaseVersionMismatchEventArgs>(this.FinalApplicationPFAWindowsFormsApplication_DatabaseVersionMismatch);
+            this.CustomizeLanguagesList += new System.EventHandler<DevExpress.ExpressApp.CustomizeLanguagesListEventArgs>(this.FinalApplicationPFAWindowsFormsApplication_CustomizeLanguagesList);
+
+            ((System.ComponentModel.ISupportInitialize)(this)).EndInit();
+
+        }
+
+        #endregion
+
+        private DevExpress.ExpressApp.SystemModule.SystemModule module1;
+        private DevExpress.ExpressApp.Win.SystemModule.SystemWindowsFormsModule module2;
+        private FinalApplicationPFA.Module.FinalApplicationPFAModule module3;
+        private FinalApplicationPFA.Module.Win.FinalApplicationPFAWindowsFormsModule module4;
+        private DevExpress.ExpressApp.Security.SecurityModule securityModule1;
+        private DevExpress.ExpressApp.Security.SecurityStrategyComplex securityStrategyComplex1;
+        private DevExpress.ExpressApp.Security.AuthenticationStandard authenticationStandard1;
+        private DevExpress.ExpressApp.ConditionalAppearance.ConditionalAppearanceModule conditionalAppearanceModule;
+        private DevExpress.ExpressApp.Dashboards.DashboardsModule dashboardsModule;
+        private DevExpress.ExpressApp.Dashboards.Win.DashboardsWindowsFormsModule dashboardsWindowsFormsModule;
+        private DevExpress.ExpressApp.FileAttachments.Win.FileAttachmentsWindowsFormsModule fileAttachmentsWindowsFormsModule;
+        private DevExpress.ExpressApp.Notifications.NotificationsModule notificationsModule;
+        private DevExpress.ExpressApp.Notifications.Win.NotificationsWindowsFormsModule notificationsWindowsFormsModule;
+        private DevExpress.ExpressApp.StateMachine.StateMachineModule stateMachineModule;
+        private DevExpress.ExpressApp.TreeListEditors.TreeListEditorsModuleBase treeListEditorsModuleBase;
+        private DevExpress.ExpressApp.TreeListEditors.Win.TreeListEditorsWindowsFormsModule treeListEditorsWindowsFormsModule;
+        private DevExpress.ExpressApp.Validation.ValidationModule validationModule;
+        private DevExpress.ExpressApp.Validation.Win.ValidationWindowsFormsModule validationWindowsFormsModule;
+        private DevExpress.ExpressApp.ViewVariantsModule.ViewVariantsModule viewVariantsModule;
+    }
+}
